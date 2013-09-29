97752d92c8ae87d3013df07d459429cdb006923b
namiltd <namiltd@users.sf.net>
2013-09-29 20:16:53 +0200

    add printer message

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,11 @@ phpSysInfo 4.0.0
  - [NEW] Rewrite the frontend with Bootstrap3 and Transparency (JSON)
  - [NEW] Frontend focused on information, much faster, much easier
 
+phpSysInfo 3.1.8
+----------------
+
+ - [ADD] Add printers messages in the XML output
+
 phpSysInfo 3.1.7
 ----------------
 
--- plugins/snmppinfo/class.snmppinfo.inc.php
+++ plugins/snmppinfo/class.snmppinfo.inc.php
@@ -57,12 +57,15 @@ class SNMPPInfo extends PSI_Plugin
                         CommonFunctions::executeProgram("snmpwalk", "-Ona -c public -v 1 ".$printer." 1.3.6.1.2.1.1.5", $buffer, PSI_DEBUG);
                         if (strlen(trim($buffer)) > 0) {
                             $this->_filecontent[$printer] = $buffer;
-                               CommonFunctions::executeProgram("snmpwalk", "-Ona -c public -v 1 ".$printer." 1.3.6.1.2.1.43.11.1.1", $buffer2, PSI_DEBUG);
+                            
+                            CommonFunctions::executeProgram("snmpwalk", "-Ona -c public -v 1 ".$printer." 1.3.6.1.2.1.43.11.1.1", $buffer2, PSI_DEBUG);
                             if (strlen(trim($buffer2)) > 0) {
-                               $this->_filecontent[$printer] = $buffer."\n".$buffer2;
-                            } else {
-                                $this->_filecontent[$printer] = $buffer;
-                            }
+                               $this->_filecontent[$printer] = $this->_filecontent[$printer]."\n".$buffer2;
+                            } 
+                            CommonFunctions::executeProgram("snmpwalk", "-Ona -c public -v 1 ".$printer." 1.3.6.1.2.1.43.18.1.1", $buffer3, PSI_DEBUG);
+                            if (strlen(trim($buffer3)) > 0) {
+                               $this->_filecontent[$printer] =  $this->_filecontent[$printer]."\n".$buffer3;
+                            } 
                         }
                     }
                 }
@@ -86,6 +89,7 @@ class SNMPPInfo extends PSI_Plugin
                                 $buffer=$buffer.$id." = ".$string."\n";
                             }
                             if (! PSI_DEBUG) restore_error_handler();
+                            
                             $bufferarr2=snmprealwalk($printer, "public", "1.3.6.1.2.1.43.11.1.1");
                             if (! PSI_DEBUG) set_error_handler('errorHandlerPsi');
                             if (! empty($bufferarr2)) {
@@ -93,6 +97,15 @@ class SNMPPInfo extends PSI_Plugin
                                     $buffer=$buffer.$id." = ".$string."\n";
                                 }
                             }
+                            
+                            $bufferarr3=snmprealwalk($printer, "public", "1.3.6.1.2.1.43.18.1.1");
+                            if (! PSI_DEBUG) set_error_handler('errorHandlerPsi');
+                            if (! empty($bufferarr3)) {
+                                foreach ($bufferarr3 as $id=>$string) {
+                                    $buffer=$buffer.$id." = ".$string."\n";
+                                }
+                            }
+
                             if (strlen(trim($buffer)) > 0) {
                                 $this->_filecontent[$printer] = $buffer;
                             }
@@ -150,7 +163,10 @@ class SNMPPInfo extends PSI_Plugin
                     $this->_result[$printer][$data[1]]['prtMarkerSuppliesLevel']=$data[2];
                 }
                 if (preg_match('/^\.1\.3\.6\.1\.2\.1\.1\.5\.0 = STRING:\s(.*)/', $line, $data)) {
-                    $this->_result[$printer][0]['prtMarkerSuppliesDescription']=$data[1];
+                    $this->_result[$printer][0]['prtMarkerSuppliesDescription']=trim($data[1],"\"");;
+                }
+                if (preg_match('/^\.1\.3\.6\.1\.2\.1\.43\.18\.1\.1\.8\.1\.(.*) = STRING:\s(.*)/', $line, $data)) {
+                    $this->_result[$printer][99][]=trim($data[2],"\"");
                 }
             }
         }
@@ -167,14 +183,25 @@ class SNMPPInfo extends PSI_Plugin
             $xmlsnmppinfo_printer = $this->xml->addChild("Printer");
             $xmlsnmppinfo_printer->addAttribute("Device", $printer);
             foreach ($markersupplies_item as $marker=>$snmppinfo_item) {
+                
+                
                 if ($marker==0) {
                     $xmlsnmppinfo_printer->addAttribute("Name", $snmppinfo_item['prtMarkerSuppliesDescription']);
-                } else {
+                } 
+                else if ($marker==99) {
+                    foreach($snmppinfo_item as $item) {
+                        $xmlsnmppinfo_errors = $xmlsnmppinfo_printer->addChild("PrinterMessage");
+                        $xmlsnmppinfo_errors->addAttribute("Message",$item);
+                    }
+               }
+               else {
                     $xmlsnmppinfo = $xmlsnmppinfo_printer->addChild("MarkerSupplies");
+                    
                     if (isset($snmppinfo_item['prtMarkerSuppliesDescription']))
                         $xmlsnmppinfo->addAttribute("Description", $snmppinfo_item['prtMarkerSuppliesDescription']);
                     else
                         $xmlsnmppinfo->addAttribute("Description",""); /* empty on some devices */
+                        
                     $xmlsnmppinfo->addAttribute("SupplyUnit", $snmppinfo_item['prtMarkerSuppliesSupplyUnit']);
                     $xmlsnmppinfo->addAttribute("MaxCapacity", $snmppinfo_item['prtMarkerSuppliesMaxCapacity']);
                     $xmlsnmppinfo->addAttribute("Level", $snmppinfo_item['prtMarkerSuppliesLevel']);
@@ -197,6 +224,11 @@ class SNMPPInfo extends PSI_Plugin
             foreach ($markersupplies_item as $marker=>$snmppinfo_item) {
                 if ($marker==0) {
                     $data['Name'] = $snmppinfo_item['prtMarkerSuppliesDescription'];
+                } 
+                else if ($marker==99) {
+                    foreach($snmppinfo_item as $item) {
+                        $snmp['Message'][] = $item;
+                    }
                 } else {
                     $subdata = array(
                         'SupplyUnit' => $snmppinfo_item['prtMarkerSuppliesSupplyUnit'],
